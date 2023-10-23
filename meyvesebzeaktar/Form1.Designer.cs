e6614212001235a00d8cdbf4614bb3a36a7a61e3
kevrrana <145452208+kevrrana@users.noreply.github.com>
2023-10-23 16:01:07 +0300

    Add files via upload

---
+++ meyvesebzeaktar/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace meyvesebzeaktar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstYiyecekler = new System.Windows.Forms.ListBox();
+            this.lstSebze = new System.Windows.Forms.ListBox();
+            this.lstMeyve = new System.Windows.Forms.ListBox();
+            this.btnAktar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(50, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Yiyecekler";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(190, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Meyve";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(329, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sebze";
+            // 
+            // lstYiyecekler
+            // 
+            this.lstYiyecekler.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstYiyecekler.FormattingEnabled = true;
+            this.lstYiyecekler.ItemHeight = 20;
+            this.lstYiyecekler.Items.AddRange(new object[] {
+            "kiraz",
+            "erik",
+            "çilek",
+            "brokoli",
+            "prasa",
+            "patlıcan"});
+            this.lstYiyecekler.Location = new System.Drawing.Point(54, 123);
+            this.lstYiyecekler.Name = "lstYiyecekler";
+            this.lstYiyecekler.Size = new System.Drawing.Size(100, 184);
+            this.lstYiyecekler.TabIndex = 3;
+            // 
+            // lstSebze
+            // 
+            this.lstSebze.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstSebze.FormattingEnabled = true;
+            this.lstSebze.ItemHeight = 20;
+            this.lstSebze.Location = new System.Drawing.Point(333, 123);
+            this.lstSebze.Name = "lstSebze";
+            this.lstSebze.Size = new System.Drawing.Size(100, 184);
+            this.lstSebze.TabIndex = 4;
+            // 
+            // lstMeyve
+            // 
+            this.lstMeyve.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstMeyve.FormattingEnabled = true;
+            this.lstMeyve.ItemHeight = 20;
+            this.lstMeyve.Location = new System.Drawing.Point(194, 123);
+            this.lstMeyve.Name = "lstMeyve";
+            this.lstMeyve.Size = new System.Drawing.Size(100, 184);
+            this.lstMeyve.TabIndex = 5;
+            // 
+            // btnAktar
+            // 
+            this.btnAktar.BackColor = System.Drawing.Color.Lime;
+            this.btnAktar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAktar.Location = new System.Drawing.Point(54, 334);
+            this.btnAktar.Name = "btnAktar";
+            this.btnAktar.Size = new System.Drawing.Size(103, 31);
+            this.btnAktar.TabIndex = 6;
+            this.btnAktar.Text = "Aktar";
+            this.btnAktar.UseVisualStyleBackColor = false;
+            this.btnAktar.Click += new System.EventHandler(this.btnAktar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAktar);
+            this.Controls.Add(this.lstMeyve);
+            this.Controls.Add(this.lstSebze);
+            this.Controls.Add(this.lstYiyecekler);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstYiyecekler;
+        private System.Windows.Forms.ListBox lstSebze;
+        private System.Windows.Forms.ListBox lstMeyve;
+        private System.Windows.Forms.Button btnAktar;
+    }
+}
+
---
+++ meyvesebzeaktar/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace meyvesebzeaktar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAktar_Click(object sender, EventArgs e)
+        {
+            for (int i=0; i<lstYiyecekler.Items.Count; i++)
+                if (lstYiyecekler.Items[i].ToString()=="kiraz"||
+                    lstYiyecekler.Items[i].ToString() == "erik" ||
+                    lstYiyecekler.Items[i].ToString() == "çilek")
+                {
+                    lstMeyve.Items.Add(lstYiyecekler.Items[i]);
+                }
+                else
+                {
+                    lstSebze.Items.Add(lstYiyecekler.Items[i]);
+                }
+            }
+        }
+    }
+
